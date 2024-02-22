4455481986d438e325c767a25598f53ade4e89cd
snhrayk <ayaka21sunahara@gmail.com>
2024-02-22 16:15:00 +0900

    florita追加

--- src/components/windowPages/works/index.tsx
+++ src/components/windowPages/works/index.tsx
@@ -32,15 +32,15 @@ export default function Works() {
           <button onClick={handleShowHerstel}>詳細を見る</button>
         </div>
       </section>
-      <section>
+      <section className={styles.florita}>
         <div className={styles.imgArea}></div>
-        <h2>herstel</h2>
+        <h2>florita</h2>
         <p className={styles.explain}>
-          今子育てに奮闘中の主婦の方に向けた紅茶定期便サービス。
-          子育ての疲れに効果的な紅茶をご用意しております。
+          女性に向けた服・コスメのランダムショッピング 通販（EC）アプリ。
+          初めてのチーム制作。
         </p>
         <div className={styles.btmWrap}>
-          <p className={styles.workDate}>1年前期</p>
+          <p className={styles.workDate}>夏期講習(SIC)</p>
           <button>詳細を見る</button>
         </div>
       </section>
