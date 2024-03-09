be31f79422b8a8baacc5a533dd869f107a312652
snhrayk <ayaka21sunahara@gmail.com>
2024-03-09 12:49:55 +0900

    placeholder

--- src/components/windowPages/contact.tsx
+++ src/components/windowPages/contact.tsx
@@ -2,13 +2,17 @@ import styles from "../../styles/contact.module.scss";
 export default function Contact() {
   return (
     <div className={styles.contactWrap}>
+      {/* <p className={styles.contactText}>
+        ご連絡等がございましたら、下記かまたは
+        ayaka21sunahara@gmail.comまでお願いいたします。
+      </p> */}
       <div className={styles.to}>
         <p>from:</p>
-        <input type="text" />
+        <input type="text" placeholder="" />
       </div>
       <div className={styles.subject}>
         <p>subject:</p>
-        <input type="text" />
+        <input type="text" placeholder="" />
       </div>
       <div className={styles.msgArea}>
         <textarea name="text" id="text"></textarea>
