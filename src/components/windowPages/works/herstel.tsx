4ae31421623cc0edfc2d9518832816b228b06aa6
snhrayk <ayaka21sunahara@gmail.com>
2024-03-11 20:27:51 +0900

    herstel作成中

--- src/components/windowPages/works/herstel.tsx
+++ src/components/windowPages/works/herstel.tsx
@@ -5,48 +5,55 @@ export default function Herstel() {
     <div className={styles.container}>
       <header>
         <h1>
-          <img src="../../image/portfolio-logo.png" alt="ポートフォリオロゴ" />
+          <img src="../../image/portfolio-logo.png" alt="ポートフォリオロゴ" />{" "}
         </h1>
       </header>
-      <div className={styles.worksList}>
-        <h2>個人制作・チーム制作</h2>
-        <p>herstel</p>
-        <p>florita</p>
-        <p>fave#</p>
-        <p>ゆるめ旅</p>
-        <h2>グラフィックデザイン</h2>
-        <p>キャンドルナイトポスター</p>
-        <p>名刺</p>
-      </div>
-      <div className={styles.herstelWrap}>
-        <div className={styles.imgWrap}></div>
-        <div className={styles.imgDetail}>
-          <p>
-            +E展 敢闘賞受賞作品<span>#1年前期 #個人制作</span>
-          </p>
-          <h3>
-            herstel<span>ハーステル</span>
-          </h3>
-          <p>
-            今子育てに奮闘中の主婦の方に向けた紅茶定期便サービス。
-            子育ての疲れに効果的な紅茶をご用意しております。
-          </p>
-          <p>
-            紅茶×定期便をテーマに、企画からデザイン・Webサイトまで行いました。
-            初めての制作だったため、
-          </p>
+      <div className={styles.flex}>
+        <div className={styles.worksList}>
+          <h2>作品一覧</h2>
           <div>
-            <p>1年前期</p>
-            <p>
-              <a href=""></a>
-            </p>
+            <p>個人制作・チーム制作</p>
+            <p className={styles.selected}>herstel</p>
+            <p>florita</p>
+            <p>fave#</p>
+            <p>ゆるめ旅</p>
           </div>
-          <p>使用言語：HTML, CSS</p>
           <div>
-            使用ツール：
-            <div className={styles.tools}></div>
-            <div className={styles.tools}></div>
-            <div className={styles.tools}></div>
+            <p>グラフィックデザイン</p>
+            <p>キャンドルナイトポスター</p>
+            <p>名刺</p>
+          </div>
+        </div>
+        <div className={styles.herstelWrap}>
+          <div className={styles.imgWrap}></div>
+          <div className={styles.imgDetail}>
+            <p>
+              +E展 敢闘賞受賞作品<span>#1年前期 #個人制作</span>
+            </p>
+            <h3>
+              herstel<span>ハーステル</span>
+            </h3>
+            <p>
+              今子育てに奮闘中の主婦の方に向けた紅茶定期便サービス。
+              子育ての疲れに効果的な紅茶をご用意しております。
+            </p>
+            <p>
+              紅茶×定期便をテーマに、企画からデザイン・Webサイトまで行いました。
+              初めての制作だったため、
+            </p>
+            <div>
+              <p>1年前期</p>
+              <p>
+                <a href=""></a>
+              </p>
+            </div>
+            <p>使用言語：HTML, CSS</p>
+            <div>
+              使用ツール：
+              <div className={styles.tools}></div>
+              <div className={styles.tools}></div>
+              <div className={styles.tools}></div>
+            </div>
           </div>
         </div>
       </div>
